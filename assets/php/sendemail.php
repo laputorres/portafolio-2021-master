980026ff41390782d3e371c1e3d8efbab40c52c1
lapu torres~ <lapu.torres@gmail.com>
2022-08-29 11:11:35 -0500

    falta links redes y formulario

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -6,7 +6,7 @@
 	$message = @trim(stripslashes($_POST['message']));
 
 	$email_from = $email;
-	$email_to = 'antonioalfa22@gmail.com';
+	$email_to = 'lapu.torres@gmail.com';
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
@@ -18,5 +18,5 @@
 <html lang="en-US">
 <head>
 	<script>alert("Gracias por contactar conmigo. Te responderé lo más rápido posible".);</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=http://antoniopg.esy.es">
+	<meta HTTP-EQUIV="REFRESH" content="0; url=http://abrahamtorres.ml">
 </head>
